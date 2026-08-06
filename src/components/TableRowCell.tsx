40e0eb2b69a1b9f365c9d25cf1c0a0d07c7bebcc
devg1120 <devg1120@gmail.com>
2026-08-07 08:12:44 +0900

    up

--- src/components/TableRowCell.tsx
+++ src/components/TableRowCell.tsx
@@ -9,7 +9,7 @@ interface Props<T> {
   item: T;
   column: IColumnType<T>;
 }
-
+/*
 const TableCell = styled("td", {
   paddingTop: 0,
   paddingBottom: 0,
@@ -34,6 +34,36 @@ const TableCell = styled("td", {
   color: "black",
   border: "solid gray 1px"
 });
+*/
+
+const default_style = {
+  paddingTop: 0,
+  paddingBottom: 0,
+  paddingLeft: 5,
+  paddingRight: 5,
+
+  //fontFamily: 'sams-serif',  
+  //fontFamily: 'monospace',  
+  fontFamily: 'Inter',  
+
+  fontWeight: '400',     
+  fontSize: 15,
+
+  verticalAlign: 'middle',
+  //verticalAlign: 'bottom',
+  //verticalAlign: 'top', 
+  
+  //textAlign: 'center',     
+  textAlign: 'left',     
+  //textAlign: 'right',     
+
+  color: "black",
+  border: "solid gray 1px"
+};
+
+//const TableCell = styled("td", default_style);
+
+
 /*
     <TableCell className="tableCell" contentEditable={"plaintext-only"} suppressContentEditableWarning={true} >{column.render ? column.render(column, item) : value}</TableCell>
 */
@@ -42,6 +72,9 @@ export function TableRowCell<T>({ item, column , edit, handleChange}: Props<T>):
   const value = get(item, column.key);
   //const [value, setValue] = useState(get(item, column.key));
   //console.log("Cell", value);
+  
+  const _style = { backgroundColor : "red" }
+  const TableCell = styled("td", {...default_style, ..._style});
 
   /*
   useEffect(() => {
