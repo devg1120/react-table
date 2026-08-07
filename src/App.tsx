06cc21649eaceffa21bb4c1f8805efca869de71f
devg1120 <devg1120@gmail.com>
2026-08-07 18:10:20 +0900

    up

--- src/App.tsx
+++ src/App.tsx
@@ -106,12 +106,44 @@ const cellStyle = {
   fontFamily: 'monospace',
   fontSize: 14,
   padding: 6,
+  textAlign: 'center',
 }
 
+const localStorageName4 = "table_data4";
+const columns_sy: IColumnType<IData>[] = [
+  { key:  "A",  title: "A", width:100 },
+  { key:  "B",  title: "B", width:100 },
+  { key:  "C",  title: "C", width:100 },
+  { key:  "D",  title: "D", width:100 },
+  { key:  "E",  title: "E", width:100 },
+  { key:  "G",  title: "G", width:100 },
+];
+
+const cellStyle_sy = {
+  //backgroundColor: "red",
+  fontFamily: 'sams-serif',  
+  //fontFamily: 'monospace',
+  fontWeight: '800', 
+  fontSize: 16,
+  textAlign: 'center',
+}
+
+let data_sy = []
+for ( let x = 1 ; x <= 100 ; x++) {
+   let row_data = {}
+   for (let i in columns_sy) {
+        //console.dir(columns[i].key);
+        row_data[columns_sy[i].key] =  String(x) + "-" + columns_sy[i].key 
+   }
+   data_sy.push(row_data)
+}
 
 export const App = () => {
   return (
    <>
+     <h2> TABLE 4  scroll-y</h2>
+     <Table id="ASTable4" data={data_sy} columns={columns_sy} cellStyle={cellStyle_sy} localStorageName={localStorageName4}/>
+
      <h2> TABLE 1  </h2>
      <Table id="ASTable" data={data} columns={columns} localStorageName={localStorageName} tableStyle={tableStyle} cellStyle={cellStyle} checkColEnable={true}/>
      <h2> TABLE 2  </h2>
